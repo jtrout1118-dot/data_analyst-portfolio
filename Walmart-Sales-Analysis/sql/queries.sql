f4b6e5a00b0ebcc8fad353280ff5bf998acd702e
Joshua Trout <jtrout1118@gmail.com>
2026-04-04 07:24:15 -0400

    Update queries.sql

--- Walmart-Sales-Analysis/sql/queries.sql
+++ Walmart-Sales-Analysis/sql/queries.sql
@@ -1,1 +1,13 @@
+SELECT Store, SUM(Weekly_Sales) AS total_sales
+FROM walmart
+GROUP BY Store
+ORDER BY total_sales DESC;
 
+SELECT Holiday_Flag, AVG(Weekly_Sales) AS avg_sales
+FROM walmart
+GROUP BY Holiday_Flag;
+
+SELECT Store, AVG(Weekly_Sales) AS avg_weekly_sales
+FROM walmart
+GROUP BY Store
+ORDER BY avg_weekly_sales DESC;
